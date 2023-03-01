ae209df153f5978622042d7a8e6e203c0b38d918
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-03-02 00:27:11 +0530

    trying

--- prac1lab/5b.f90
+++
@@ -1,9 +0,0 @@
-program value2
-implicit none 
-real::a=5.0,b=4.0,c=3.0
-
-print*, a*b+(1/c)
-print*, (a/b)+(1/c)
-print*, (b/a)+(1/c)
-
-end program value2
--- prac1lab/6.f90
+++
@@ -1,8 +0,0 @@
-program logarithm
-implicit none
-
-print *, (log(1+sqrt(2.0))/3.0) +((2+ sqrt(2.0))/15.0)
-
-
-
-end program logarithm
