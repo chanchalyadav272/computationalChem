8f6cebf90c7815bd62aa79fdfd2313ee63e29441
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-03-02 00:34:53 +0530

    delete

--- eval1/1_module.f90
+++
@@ -1,8 +0,0 @@
-module sch
-    real, parameter:: pi = acos(-1.0)
-    integer::num=100,l,i,k,c
-    real::xmin=0.01,xmax=15,delx,x,factorial
-    complex::h
-    real, dimension(:), allocatable::j
-    real, dimension(:), allocatable::n
-end module sch
--- prac4/1_module.f90
+++
@@ -1,8 +0,0 @@
-module ip
-    real, parameter:: pi = acos(-1.0)
-    integer::num,i,j,k,c
-    real::a
-    real, dimension(:), allocatable::x
-    real, dimension(:), allocatable::y
-    real, dimension(:), allocatable::z
-end module ip
