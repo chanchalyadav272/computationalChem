e7dc545ea40bc0a005af47f52443ad7069712f00
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-03-29 02:48:46 +0530

    sec

--- findingRoots/t2/secant.f90
+++ findingRoots/t2/secant.f90
@@ -1,7 +1,7 @@
 module se
     integer(kind=8)::i,j,k
     real(kind=8), dimension(:,:),allocatable::x
-    real(kind=8)::x0=4,x1=2,f,e
+    real(kind=8)::x0=-3.00001,x1=0,f,e
 end module se
 program root
     use se
@@ -28,15 +28,20 @@ program root
         call func(x(i,2))
         x(i,3)=f
 
-        if(i==9) then
+        x(i,4) = abs((x(i,2)-x(i-1,2))/x(i,2))
+        if(x(i,4)<=e) then
             exit
         endif
+
+        ! if(i==9) then
+        !     exit
+        ! endif
 
         i=i+1
     enddo
 
     do j=0,i
-        write(1,*) (x(j,k),k=1,3)
+        write(1,*) (x(j,k),k=1,4)
     enddo
 
 
@@ -44,6 +49,7 @@ end program root
 subroutine func(a)
     use se
     real(kind=8)::a
-    f =  (a**2) -(4*a) -10
+    ! f =  (a**2) -(4*a) -10
+    f = a**2 + a - 2
  
 end subroutine func
